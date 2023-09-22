b17e65686b830b1d00c4bbc8af1e375350741657
TsimakuridzeNikolozi <ntsim20@freeuni.edu.ge>
2023-09-22 13:50:40 +0400

    Change

--- src/main/java/com/library/librarymanagementsystem/LibraryManagementSystemApplication.java
+++ src/main/java/com/library/librarymanagementsystem/LibraryManagementSystemApplication.java
@@ -2,6 +2,7 @@ package com.library.librarymanagementsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 public class LibraryManagementSystemApplication {
